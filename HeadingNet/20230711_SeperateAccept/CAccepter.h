e884c62e6412f550ad965697452b2cf44799378e
kjt21@kakao.com <kjtkjt21@gmail.com>
2023-07-12 05:36:30 +0900

    공개범위 변경

--- HeadingNet/20230711_SeperateAccept/CAccepter.h
+++ HeadingNet/20230711_SeperateAccept/CAccepter.h
@@ -12,9 +12,9 @@ namespace Heading
 
 		void Do_Select( );
 
-	private:
 		bool Get_NewSocket( OUT SocketList& _newSocket );
 
+	private:
 		// Accepter를 Select 용으로만 사용하고 Events 맵은 전체에서 사용하는게 맞을지 고민...
 		// 사실 어차피 Accept를 MAXIMUM이 넘도록 처리하진 않을테니 일단 여기다 선언합니다.
 		// Client만 MAX넘지 않게 처리하도록 Chat 매니저에서는 외부의 EventMap을 받아서 Select
